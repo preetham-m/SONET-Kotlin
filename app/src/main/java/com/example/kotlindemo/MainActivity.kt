49c2bc52b3cbccb8de2ff1357be6ea26525453d6
Preetham-m <m.preeetham2019@gmail.com>
2021-05-11 10:18:26 +0530

    sample kotlin android demo

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.kotlindemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txt)
+
+        btn.setOnClickListener(View.OnClickListener{
+            // Do something in response to button click
+            txt.setText("preetham")
+        })
     }
 }
